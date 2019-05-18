6f84d017ab5e2159ded388b88223e56a119d9186
Leaxiansheng <1243053844@qq.com>
2019-05-18 11:16:30 +0800

    提交github测试

---
+++ meno-admin/src/main/java/com/yunmu/menoadmin/MenoAdminApplication.java
@@ -0,0 +1,13 @@
+package com.yunmu.menoadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenoAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenoAdminApplication.class, args);
+	}
+
+}
---
+++ meno-admin/src/test/java/com/yunmu/menoadmin/MenoAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yunmu.menoadmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MenoAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
